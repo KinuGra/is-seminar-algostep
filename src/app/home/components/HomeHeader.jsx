c6b93d88ed99466aafcc9fc8a083c43a981d1295
saradasa <y.takumi0912abc@gmail.com>
2025-11-02 21:43:11 +0900

    update: HomeHeaderのtailwindをcss.moduleに置換した

--- src/app/home/components/HomeHeader.jsx
+++ src/app/home/components/HomeHeader.jsx
@@ -1,12 +1,14 @@
+import styles from './HomeHeader.module.css'
+
 export default function HomeHeader() {
   return (
     <>
       <header>
-        <div className="flex items-center space-x-3">
-          <div className="bg-blue-600 rounded-lg p-2 text-xl">💻</div>
+        <div className={styles.header}>
+          <div className={styles.bg}>💻</div>
           <div>
-            <h1 className="text-2xl font-bold">プログラミング学習</h1>
-            <p className="text-gray-600 text-sm">アルゴリズムとデータ構造を学ぼう</p>
+            <h1 className={styles.maintxt}>プログラミング学習</h1>
+            <p className={styles.subtxt}>アルゴリズムとデータ構造を学ぼう</p>
           </div>
         </div>
       </header>
