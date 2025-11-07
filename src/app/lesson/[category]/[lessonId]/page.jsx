acb70f9a2408e807c4d486038ca8351acc5a639b
air_kinu <techgit1024@gmail.com>
2025-11-07 11:05:53 +0900

    feat: レッスン画面を動的ルーティングでparamsを受け取って表示

--- src/app/lesson/[category]/[lessonId]/page.jsx
+++ src/app/lesson/[category]/[lessonId]/page.jsx
@@ -1,1 +1,23 @@
-const LessonPage = () => { };
+const LessonPage = async ({ params }) => {
+  const { category, lessonId } = await params;
+
+  if (category === "prog") {
+    return (
+      <>
+        <div>{category}</div>
+        <div>{lessonId}</div>
+      </>
+    );
+  } else if (category === "algo") {
+    return (
+      <>
+        <div>{category}</div>
+        <div>{lessonId}</div>
+      </>
+    );
+  } else {
+    return <>404</>;
+  }
+};
+
+export default LessonPage;
